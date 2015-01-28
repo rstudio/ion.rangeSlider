e1afa7417314ce27d7d8ef52afd3854c4cb7f2e1
Winston Chang <winston@stdout.org>
2015-01-28 13:54:29 -0600

    Stop propagation of click and down events

--- js/ion.rangeSlider.js
+++ js/ion.rangeSlider.js
@@ -750,6 +750,7 @@
          */
         pointerDown: function (target, e) {
             e.preventDefault();
+            e.stopPropagation();
             var x = e.pageX || e.originalEvent.touches && e.originalEvent.touches[0].pageX;
             if (e.button === 2) {
                 return;
@@ -793,6 +794,7 @@
          */
         pointerClick: function (target, e) {
             e.preventDefault();
+            e.stopPropagation();
             var x = e.pageX || e.originalEvent.touches && e.originalEvent.touches[0].pageX;
             if (e.button === 2) {
                 return;
